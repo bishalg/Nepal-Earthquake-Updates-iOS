5abe11a532aa0a6fc8db790ab17b653994391614
Bishal Ghimire <bishalghimire@gmail.com>
2016-07-27 17:32:25 +0545

    Starting Swift2 project

---
+++ iOS Project/Swift/Nepal Earthquake Updates/Nepal Earthquake Updates/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Nepal Earthquake Updates
+//
+//  Created by Bishal Ghimire on 7/27/16.
+//  Copyright © 2016 Bishal Ghimire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+}
